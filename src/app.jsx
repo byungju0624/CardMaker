64fd4b7969ef8e4fc2349ccaf9a35ee187b7cbf0
정병주 <jeongbyeongju@jeongbyeongjuui-MacBookPro.local>
2021-02-21 23:02:23 +0900

    add login and logout functionality

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import styles from "./app.module.css";
 import Login from "./components/login/login.jsx";
+import Maker from "./components/maker/maker";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 
 function App({ authService }) {
   const [isLogin, setIsLogin] = useState(false);
 
   return (
     <div className={styles.main}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
